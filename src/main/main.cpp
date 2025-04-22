14b658eeb92ace6dc134091359eaf63c197390cc
Sonic Dreamcaster <alejandro.asenjo88@gmail.com>
2025-04-22 09:36:04 -0300

    Merge branch 'main' of https://github.com/kholdfuzion/Goldenrecomp

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -323,7 +323,7 @@ RspUcodeFunc* get_rsp_microcode(const OSTask* task) {
     }
 }
 
-extern "C" void recomp_entrypoint(uint8_t * rdram, recomp_context * ctx);
+extern "C" void _start(uint8_t * rdram, recomp_context * ctx);
 gpr get_entrypoint_address();
 
 // array of supported GameEntry objects
@@ -331,11 +331,11 @@ std::vector<recomp::GameEntry> supported_games = {
     {
         .rom_hash = 0xb33d03a532ba225e, // retail
         .internal_name = "GOLDENEYE",
-        .game_id = u8"ge007.us",
+        .game_id = u8"ge007.tlbfree",
         .save_type = recomp::SaveType::Eep4k,
         .is_enabled = true,
-        .entrypoint_address = get_entrypoint_address(),
-        .entrypoint = recomp_entrypoint,
+        .entrypoint_address = 0x80000400,
+        .entrypoint = _start,
     },
 };
 
